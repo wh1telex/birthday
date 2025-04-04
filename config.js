db55b58ad8c74db09b14c14665b25e95f80bada2
wh1telex <128623700+wh1telex@users.noreply.github.com>
2025-04-04 14:27:37 +0800

    Update config.js

--- config.js
+++ config.js
@@ -40,6 +40,8 @@ var config = {
         "心爱的小欣": "imgs/IMG20240406081043.jpg",
         "今天是你的生日": "imgs/before.jpg",
         "两年前，我们还在拉拉扯扯": "imgs/Amay.jpg",
+        "直到后来":"imgs/river.jpg",
+        "相册里的你越来越多": "imgs/pic.jpg",
         "才发现我满眼是你": "imgs/eyes.jpg",
      //   "今天是你的生日": "imgs/大一生日.jpg",
     },
